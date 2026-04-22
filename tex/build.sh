bb19ce00b466159b1e45afa5f66f14def9f76e15
2qx <2qx.git@small.neomailbox.ch>
2026-04-22 07:43:21 -0500

    fix zine cover

--- tex/build.sh
+++ tex/build.sh
@@ -7,4 +7,5 @@ pdfjam cover.pdf monterey-protocols.pdf back.pdf -o monterey-protocols-A5.pdf;
 cp monterey-protocols-A5.pdf ../assets/monterey-protocols_A5.pdf;
 
 pdfjam monterey-protocols.pdf --booklet true --paper a4 --landscape --outfile monterey-protocols-zine.pdf;
-pdfjam monterey-protocols-zine_cover.pdf '1,{}' monterey-protocols-zine.pdf --paper a4 --landscape  --outfile ../assets/monterey-protocols-zine.pdf;
+pdfjam --landscape --angle 180 monterey-protocols-zine_cover.pdf '1,{}'  --outfile monterey-protocols-zine_cover-page.pdf;
+pdfjam monterey-protocols-zine_cover-page.pdf monterey-protocols-zine.pdf --paper a4 --landscape  --outfile ../assets/monterey-protocols-zine.pdf;
