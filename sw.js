52656903ea9de17ed409bb9dcb77732b9d67b65e
2qx <2qx.git@small.neomailbox.ch>
2026-04-30 09:39:53 -0500

    offline

---
+++ sw.js
@@ -0,0 +1,22 @@
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open("v1");
+    await cache.addAll(resources);
+};
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        addResourcesToCache([
+            "/",
+            "/0/",
+            "/1/",
+            "/2/",
+            "/3/",
+            "/4/",
+            "/5/",
+            "/assets/monterey-protocols_A5.pdf",
+            "/style.css",
+            "/about.html",
+            "/merc.png"
+        ]),
+    );
+});
