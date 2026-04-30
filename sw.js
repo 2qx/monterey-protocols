4feb8e510fbc54623f5bc5404c5ba8dbeebb62a3
2qx <2qx.git@small.neomailbox.ch>
2026-04-30 10:26:59 -0500

    add icon

--- sw.js
+++ sw.js
@@ -1,21 +1,20 @@
-const addResourcesToCache = async (resources) => {
-    const cache = await caches.open("v1");
-    await cache.addAll(resources);
-};
-
-self.addEventListener("install", (event) => {
+this.addEventListener("install", (event) => {
     event.waitUntil(
-        addResourcesToCache([
-            "/",
-            "/0/",
-            "/1/",
-            "/2/",
-            "/3/",
-            "/4/",
-            "/5/",
-            "/style.css",
-            "/about",
-            "/merc.png"
-        ]),
+        caches
+            .open("v1")
+            .then((cache) =>
+                cache.addAll([
+                    "/",
+                    "/0/",
+                    "/1/",
+                    "/2/",
+                    "/3/",
+                    "/4/",
+                    "/5/",
+                    "/style.css",
+                    "/about",
+                    "/merc.png"
+                ]),
+            ),
     );
 });
